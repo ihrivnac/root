7f4406b1474a4038b71f2cb36efb51a56a9aceba
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2026-04-05 11:57:08 +0200

    Adding TGeoMultiUnion.h

---
+++ geom/geom/inc/TGeoMultiUnion.h
@@ -0,0 +1,20 @@
+// @(#)root/geom:$Id$
+// Author: Mihaela Gheata   05/06/02
+
+/*************************************************************************
+ * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef ROOT_TGeoMultiUnion
+#define ROOT_TGeoMultiUnion
+
+#if defined(ROOT_USE_VECGEOM_SOLIDS)
+  #include "TGeoVGMultiUnion.h"
+  using TGeoMultiUnion = TGeoVGMultiUnion;
+#endif
+
+#endif
